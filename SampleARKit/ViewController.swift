00826f7de5466d145d1f8a90537f16256f8193e4
sakiyamaK <sakiyama.k@gmail.com>
2020-10-03 20:49:14 +0900

    Initial Commit

---
+++ SampleARKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SampleARKit
+//
+//  Created by sakiyamaK on 2020/10/03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
